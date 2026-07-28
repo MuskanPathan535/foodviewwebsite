9302019bc2d701820aff53a05bc53b657b955e03
Muskan Pathan <mp5927975@gmail.com>
2026-07-28 12:15:21 +0530

    Update CORS for deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,9 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: [
+        "http://localhost:5173"
+    ],
     credentials: true
 }));
 app.use(cookieParser());
