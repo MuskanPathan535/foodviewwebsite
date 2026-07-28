6c3115a223fd4386398ec36b4680be1dd7ac1941
Muskan Pathan <mp5927975@gmail.com>
2026-07-28 13:07:05 +0530

    updated final frontend

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -25,7 +25,11 @@ async function registerUser(req,res){
         id: user._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+   res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+   });
 
     res.status(200).json({
         message: "User logged in successfully",
@@ -63,7 +67,11 @@ async function loginUser(req, res) {
         id: user._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    });
 
     res.status(200).json({
         message: "User logged in successfully",
@@ -76,7 +84,11 @@ async function loginUser(req, res) {
 }
 
 function logoutUser(req, res) {
-    res.clearCookie("token");
+   res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+});
     res.status(200).json({
         message: "User logged out successfully"
     });
@@ -111,7 +123,11 @@ async function registerFoodPartner(req, res) {
         id: foodPartner._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    });
 
     res.status(201).json({
         message: "Food partner registered successfully",
@@ -153,7 +169,11 @@ async function loginFoodPartner(req, res) {
         id: foodPartner._id,
     }, process.env.JWT_SECRET)
 
-    res.cookie("token", token)
+    res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+   });
 
     res.status(200).json({
         message: "Food partner logged in successfully",
@@ -205,7 +225,11 @@ async function getCurrentUser(req, res) {
 }
 
 function logoutFoodPartner(req, res) {
-    res.clearCookie("token");
+    res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+});
     res.status(200).json({
         message: "Food partner logged out successfully"
     });
